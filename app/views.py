5011becef8c1f7b96cb0985bf13b50be91a297ef
s15283 <15283@eans-nt.edu.pl>
2025-04-29 10:47:52 +0200

    add extracting form

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
-def extract():
+@app.route('/extract', methods=['post'])
+def extract_data():
+    product_id = request.form.get('product_id')
+    return redirect(url_for('product', product_id=product_id))
+
+@app.route('/extract', methods=['get'])
+def display_form():
     return render_template("extract.html")
 
 @app.route('/products')
@@ -17,6 +22,6 @@ def products():
 def author():
     return render_template("author.html")
 
-@app.route('/product<id>')
-def product(product_id:int):
+@app.route('/product/<string:product_id>')
+def product(product_id: str):
     return render_template("product.html", product_id=product_id)
